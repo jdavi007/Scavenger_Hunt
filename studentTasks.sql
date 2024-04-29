5699f8fee6af330e5086120d80984728d5d67c3d
jdavi007 <158612101+jdavi007@users.noreply.github.com>
2024-04-29 15:36:04 -0400

    Update studentTasks.sql

--- studentTasks.sql
+++ studentTasks.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS studentTasks;
 
 CREATE TABLE studentTasks (
-    task_id INTEGER NOT NULL, -- task id
-    event_id INTEGER NOT NULL, -- foreign key referencing events table
-    user_id INTEGER NOT NULL, -- foreign key referencing user table
+    task_id INTEGER NOT NULL, -- Task id
+    event_id INTEGER NOT NULL, -- Foreign key referencing events table
+    user_id INTEGER NOT NULL, -- Foreign key referencing user table
     hasResponse BOOLEAN NOT NULL, -- 0=no response, 1=has response
     response TEXT,
     UNIQUE(task_id, event_id, user_id)
