960c04bab946cc0d405dd046f527094636a23a85
jdavi007 <158612101+jdavi007@users.noreply.github.com>
2024-04-29 15:32:31 -0400

    Update eventSchema.sql

--- eventSchema.sql
+++ eventSchema.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS events;
 
 CREATE TABLE events (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, --event id
-    title TEXT NOT NULL, --event title
-    imageBlob TEXT NOT NULL --cover image viewable on index pages stored as binary data
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Event id
+    title TEXT NOT NULL, -- Event title
+    imageBlob TEXT NOT NULL -- Cover image viewable on index pages stored as binary data
 );
