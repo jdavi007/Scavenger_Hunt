633f5d500ae4191e0ea247f9004b4ca49a5a98f1
jdavi007 <158612101+jdavi007@users.noreply.github.com>
2024-04-29 15:36:34 -0400

    Update taskSchema.sql

--- taskSchema.sql
+++ taskSchema.sql
@@ -3,8 +3,8 @@
 DROP TABLE IF EXISTS tasks;
 
 CREATE TABLE tasks (
-    task_id INTEGER PRIMARY KEY AUTOINCREMENT, -- task id
-    event_id INTEGER NOT NULL, -- foreign key referencing events table
-    description TEXT NOT NULL, -- task description
-    uploadType TEXT NOT NULL -- student upload type
+    task_id INTEGER PRIMARY KEY AUTOINCREMENT, -- Task id
+    event_id INTEGER NOT NULL, -- Foreign key referencing events table
+    description TEXT NOT NULL, -- Task description
+    uploadType TEXT NOT NULL -- Student upload type
 );
