42337bc74ba52a86c713d96b1d9dafef3443ca66
jdavi007 <158612101+jdavi007@users.noreply.github.com>
2024-04-29 15:37:03 -0400

    Update userSchema.sql

--- userSchema.sql
+++ userSchema.sql
@@ -3,11 +3,12 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT, --internal user id (not UVA Wise ID)
-    email NVARCHAR(40) UNIQUE NOT NULL, --UVA Wise email address
-    password NVARCHAR(64) NOT NULL, --user password
-    firstName NVARCHAR(40) NULL, --first & last name are never referenced but kept just in case
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT, -- User ID
+    email NVARCHAR(40) UNIQUE NOT NULL, -- UVA Wise email address
+    password NVARCHAR(64) NOT NULL, -- User password
+    firstName NVARCHAR(40) NULL, -- First & last name are never referenced but kept just in case
     lastName NVARCHAR(40) NULL,
-    isAdmin BOOLEAN NOT NULL -- 0=student, 1=staff
+    isAdmin BOOLEAN NOT NULL, -- 0=student, 1=staff
+    verificationToken NVARCAR(32) NULL, -- For email verification
+    verified BOOLEAN NOT NULL DEFAULT 0 -- 0=not verified, 1=verified
 );
-
